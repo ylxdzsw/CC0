e3e7cf0787e38e12c00c6705931ff7605c585547
Shiwei Zhang <ylxdzsw@gmail.com>
2021-04-20 20:08:59 +0800

    tune

--- model/poc.py
+++ model/poc.py
@@ -10,7 +10,7 @@ try:
 except:
     data = []
 
-    while len(data) < 200000:
+    while len(data) < 2000000:
         env = Environment()
         for i in range(200):
             env.random_move()
@@ -23,21 +23,18 @@ except:
 
     save(data, "data")
 
-# tokens are feed in [<CLS>, self_piece_pos_1, self_piece_pos_2, ..., oppo_piece_pos_1, oppo_piece_pos_2, ...]
-# no positiontal encoding or mask used, so the input is unordered
+# no positional encoding or mask used, so the input is unordered
 class TransformerClassifier(torch.nn.Module):
     def __init__(self, board_size):
         super(TransformerClassifier, self).__init__()
-        self._board_size = board_size
-        self.embedding = torch.nn.Embedding(board_size+1, 192-3)
-        encoder_layer = torch.nn.TransformerEncoderLayer(192, nhead=6, dim_feedforward=256)
+        self.embedding = torch.nn.Embedding(board_size*3, 256)
+        encoder_layer = torch.nn.TransformerEncoderLayer(256, nhead=8, dim_feedforward=768)
         self.encoder = torch.nn.TransformerEncoder(encoder_layer, 8)
-        self.decoder = torch.nn.Linear(192, board_size)
+        self.decoder = torch.nn.Linear(256, board_size)
         self.activation = torch.nn.Sigmoid()
 
-    def forward(self, pieces, masks):
-        x = self.embedding(pieces)
-        x = torch.cat((x, masks), 2)
+    def forward(self, X):
+        x = self.embedding(X)
         x = self.encoder(x.permute(1, 0, 2))
         x = self.decoder(x[0, :, :])
         x = self.activation(x)
@@ -46,9 +43,9 @@ class TransformerClassifier(torch.nn.Module):
 training_set = data[:-5000]
 test_set = data[-5000:]
 
+# token size: 121 * 3. for the self pieces, oppo pieces, and the focused piece. The focuses piece always feed first.
 def get_batch(dataset, batch_size, start=-1):
-    pieces = np.zeros((batch_size, 21), dtype=np.int32)
-    masks = np.zeros((batch_size, 21, 3), dtype=np.int32)
+    X = np.zeros((batch_size, 21), dtype=np.int32)
     y = np.zeros((batch_size, 121), dtype=np.float32)
 
     for i in range(batch_size):
@@ -57,40 +54,35 @@ def get_batch(dataset, batch_size, start=-1):
         else:
             index = start + i
         self_pieces, oppo_pieces, pos, moves = dataset[index]
-        pieces[i, 0] = 121 # the special token
-        for j, p in enumerate(self_pieces):
-            pieces[i, j+1] = p
-            masks[i, j+1, 0] = 1
-            if p == pos:
-                masks[i, j+1, 2] = 1
+
+        X[i, 0] = pos
+        for j, p in enumerate([p for p in self_pieces if p != pos]):
+            X[i, j+1] = p + 121
         for j, p in enumerate(oppo_pieces):
-            pieces[i, j+11] = p
-            masks[i, j+11, 1] = 1
-            if p == pos:
-                masks[i, j+11, 2] = 1
+            X[i, j+11] = p + 121*2
         for p in moves:
             y[i, p] = 1
 
-    return pieces, masks, y
+    return X, y
 
 model = TransformerClassifier(121).cuda()
 loss = torch.nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=3e-5)
-best = 0
+optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
+best = .1
 
-for epoch in range(50000):
+for epoch in range(500000):
     model.train()
-    pieces, masks, y = get_batch(training_set, 256)
-    p = model(torch.from_numpy(pieces).cuda(), torch.from_numpy(masks).cuda())
+    X, y = get_batch(training_set, 128)
+    p = model(torch.from_numpy(X).cuda())
     L = loss(p, torch.from_numpy(y).cuda())
     L.backward()
     torch.nn.utils.clip_grad_norm_(model.parameters(), .6)
     optimizer.step()
-    if epoch % 100 == 0:
+    if epoch % 200 == 0:
         model.eval()
         with torch.no_grad():
-            pieces, masks, y = get_batch(test_set, 5000, 0)
-            p = model(torch.from_numpy(pieces).cuda(), torch.from_numpy(masks).cuda())
+            X, y = get_batch(test_set, 5000, 0)
+            p = model(torch.from_numpy(X).cuda())
             p = (p > .5).cpu().numpy()
             acc = sum(np.sum(p == y, 1) == 121) / 5000
             print("epoch {}, loss {:.3g}, acc {:.3g}".format(epoch, L.item(), acc))
