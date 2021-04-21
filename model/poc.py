08312accd0da1b31026790b91de57aeab6fc7857
Shiwei Zhang <ylxdzsw@gmail.com>
2021-04-21 12:07:19 +0800

    resume from checkpoint. Reachicing 60% test accuracy

--- model/poc.py
+++ model/poc.py
@@ -2,6 +2,7 @@
 
 import torch
 import numpy as np
+import sys
 from utils import save, load, normalize
 from environment import Environment
 
@@ -67,10 +68,20 @@ def get_batch(dataset, batch_size, start=-1):
 
 model = TransformerClassifier(121).cuda()
 loss = torch.nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
-best = .1
+optimizer = torch.optim.Adam(model.parameters(), lr=3e-5)
+best = .5
+epoch = 0
 
-for epoch in range(500000):
+try:
+    checkpoint = torch.load(sys.argv[1])
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    best = checkpoint['acc']
+except:
+    pass
+
+while epoch < 800000:
     model.train()
     X, y = get_batch(training_set, 128)
     p = model(torch.from_numpy(X).cuda())
@@ -78,6 +89,7 @@ for epoch in range(500000):
     L.backward()
     torch.nn.utils.clip_grad_norm_(model.parameters(), .6)
     optimizer.step()
+    epoch += 1
     if epoch % 200 == 0:
         model.eval()
         with torch.no_grad():
